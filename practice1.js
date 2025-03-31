69cd80001cd7a071a87bb96afe7f232efb3fa896
Elizabeth Clawson <emclawson1@gmail.com>
2025-03-31 15:16:01 +0530

    solved problem 1

---
+++ practice1.js
@@ -0,0 +1,13 @@
+//1 - print all even numbers from 0 - 10 
+
+ function printEvens(){
+    for (let i = 0; i <= 10; i++){
+        if (i%2 === 0) {
+            console.log(i);
+        }
+    };
+ };
+
+ printEvens();
+
+ //2 - 
