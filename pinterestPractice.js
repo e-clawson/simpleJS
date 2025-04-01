f56c6f46418a37e09f774846d1336e0843e8c07d
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-01 15:45:57 +0530

    added instructinos for Q 4 practice

---
+++ pinterestPractice.js
@@ -0,0 +1,24 @@
+//Q1 
+
+//Q2
+
+//Q3 
+
+//Q4 
+
+//Q4 - sample - sumDividibleByK 
+
+/* you are given an array of integers a and an intefer k.
+your task is to calculate the number of ways to pick teo different 
+indicies i < j, such that a[i] + a[j] is divisible by k.*/
+
+/* example: 
+for a = [1,2,3,4,5] and k = 3, the output should be: 
+sumsDivisibleByK(a,k) = 4;
+
+there are 4 pairs of numbers that sum to a multiple of k = 3: 
+a[0] + a[1] = 1 + 2 = 3
+a[0] + a[4] = 1 + 5 = 6
+a[1] + a[3] = 2 + 4 = 6
+a[3] + a[4] = 4 + 5 = 9
+*/
